9e453fdbfddc539c665e11abca67f90150d9179c
sergiu-sala <121624777+Sala-Sergiu@users.noreply.github.com>
2023-09-05 20:58:57 +0300

    making the layout

--- src/App.js
+++ src/App.js
@@ -1,3 +1,26 @@
+import { useState } from "react";
+
 export default function App() {
-  return <div>test</div>;
+  return (
+    <div className="container">
+      <LeftBox></LeftBox>
+      <RightBox></RightBox>
+    </div>
+  );
+}
+
+function LeftBox() {
+  return (
+    <section className="left">
+      <h1>Left side</h1>
+    </section>
+  );
+}
+
+function RightBox() {
+  return (
+    <section className="right">
+      <h1>Right size</h1>
+    </section>
+  );
 }

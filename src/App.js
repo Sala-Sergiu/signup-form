eaf70d6dd19f4b824fb6d2c2d750505b040ae801
sergiu-sala <121624777+Sala-Sergiu@users.noreply.github.com>
2023-09-08 20:50:22 +0300

    worked on the layout

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ export default function App() {
 function LeftBox() {
   return (
     <section className="left">
-      <h1>Left side</h1>
+      <Logo />
     </section>
   );
 }
@@ -20,7 +20,108 @@ function LeftBox() {
 function RightBox() {
   return (
     <section className="right">
-      <h1>Right size</h1>
+      <Text>
+        Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in
+        laying out print, graphic or web designs. The passage is attributed to
+        an unknown typesetter in the 15th century who is thought to have
+        scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a
+        type specimen book. It usually begins with:
+      </Text>
+
+      <InputBox />
+      <Button />
+      <Text>
+        If you alreadu have an account, then you can <span>log in</span>
+      </Text>
     </section>
   );
+}
+
+function Logo() {
+  return (
+    <div className="qwerty">
+      <h1 className="logo">Logo</h1>
+    </div>
+  );
+}
+
+function Text({ children }) {
+  return (
+    <div>
+      <p>{children}</p>
+    </div>
+  );
+}
+function InputBox() {
+  return (
+    <>
+      <h2>Let's do this</h2>
+      <div className="input-grid">
+        <div>
+          <label for="firstName" className="label">
+            First Name:
+          </label>
+          <input
+            type="text"
+            id="firstName"
+            name="firstName"
+            className="input"
+          ></input>
+        </div>
+        <div>
+          <label for="lastName" className="label">
+            Last Name:
+          </label>
+          <input
+            type="text"
+            id="lastName"
+            name="lastName"
+            className="input"
+          ></input>
+        </div>
+        <div>
+          <label for="email" className="label">
+            Email:
+          </label>
+          <input type="email" id="email" name="email" className="input"></input>
+        </div>
+        <div>
+          <label for="phoneNum" className="label">
+            Phone Number:
+          </label>
+          <input
+            type="tel"
+            id="phoneNum"
+            name="phoneNum"
+            className="input"
+          ></input>
+        </div>
+        <div>
+          <label for="password" className="label">
+            Password:
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            className="input"
+          ></input>
+        </div>
+        <div>
+          <label for="Cpassword" className="label">
+            Confirm Password:
+          </label>
+          <input
+            type="password"
+            id="Cpassword"
+            name="Cpassword"
+            className="input"
+          ></input>
+        </div>
+      </div>
+    </>
+  );
+}
+function Button() {
+  return <button>Sing up!</button>;
 }
